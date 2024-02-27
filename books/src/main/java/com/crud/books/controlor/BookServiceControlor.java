7b44de62d8cbecd7f8cbd2a13ad1dcfad58799d4
Ankushk-01 <ankushkumar.me24@gmail.com>
2024-02-28 00:24:59 +0530

    added the controlor of put request

---
+++ books/src/main/java/com/crud/books/controlor/BookServiceControlor.java
@@ -0,0 +1,32 @@
+package com.crud.books.controlor;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.crud.books.domain.Book;
+import com.crud.books.services.BookServices;
+
+@Controller
+public class BookServiceControlor {
+
+    private final BookServices bookServices;
+
+    @Autowired
+    public BookServiceControlor(BookServices bookServices) {
+        this.bookServices = bookServices;
+    }
+
+    @PutMapping(path = "/books/{isbn}")
+    public ResponseEntity<Book> createBook(@PathVariable final String isbn, @RequestBody Book book) {
+        book.setIsbn(isbn);
+        final Book savedBook = bookServices.create(book);
+        ResponseEntity<Book> response = new ResponseEntity<Book>(savedBook, HttpStatus.CREATED);
+        return response;
+    }
+
+}
--- books/src/test/java/com/crud/books/services/Impl/BookServiceImplTest.java
+++ books/src/test/java/com/crud/books/services/Impl/BookServiceImplTest.java
@@ -4,6 +4,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 
@@ -19,7 +20,6 @@ import com.crud.books.repository.BooksRepository;
 public class BookServiceImplTest {
     @Mock
     private BooksRepository bookRepository;
-
     @InjectMocks
     private BookServiceImpl bookServiceImpl;
     @Test
@@ -27,8 +27,17 @@ public class BookServiceImplTest {
         final Book book = Book.builder().isbn("597796556").author("John Ronald Reuel Tolkien").title("Lord of the Rings").build();
         final BookEntity bookEntity = BookEntity.builder().isbn("597796556").author("John Ronald Reuel Tolkien").title("Lord of the Rings").build();
 
-        when(bookRepository.save(eq(bookEntity))).thenReturn(bookEntity);
-        Book result = bookServiceImpl.create(book);
+        // when(bookRepository.save(eq(bookEntity))).thenReturn(bookEntity);
+        // when(bookRepository.save(any(BookEntity.class))).thenReturn(bookEntity);
+        // Book result = bookServiceImpl.create(book);
+         // Debug print statement
+    System.out.println("Debug: Before when(bookRepository.save)");
+
+    when(bookRepository.save(any(BookEntity.class))).thenReturn(bookEntity);
+
+    // Debug print statement
+    System.out.println("Debug: After when(bookRepository.save)");
+    Book result = bookServiceImpl.create(book);
         assertEquals(book,result);
     }
 }
