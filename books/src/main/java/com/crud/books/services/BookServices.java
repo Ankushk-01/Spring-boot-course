476201eafe37b7e3f265203aec894fb504e2fd95
Ankushk-01 <ankushkumar.me24@gmail.com>
2024-02-29 00:06:11 +0530

    Added new rest request to book service

--- books/src/main/java/com/crud/books/services/BookServices.java
+++ books/src/main/java/com/crud/books/services/BookServices.java
@@ -1,7 +1,11 @@
 package com.crud.books.services;
 
+import java.util.Optional;
+
 import com.crud.books.domain.Book;
 
 public interface BookServices {
     Book create(Book book);
+
+    Optional<Book> bookById(String isbn);
 }
