de991b25b0a45ccbc5206ae17f1cd6fc4b587c9f
Ankushk-01 <ankushkumar.me24@gmail.com>
2024-02-29 00:10:40 +0530

    Added new rest request to book service

--- books/src/main/java/com/crud/books/controlor/BookServiceControlor.java
+++ books/src/main/java/com/crud/books/controlor/BookServiceControlor.java
@@ -29,4 +29,6 @@ public class BookServiceControlor {
         return response;
     }
 
+    
+
 }
--- books/src/main/java/com/crud/books/services/Impl/BookServiceImpl.java
+++ books/src/main/java/com/crud/books/services/Impl/BookServiceImpl.java
@@ -1,5 +1,7 @@
 package com.crud.books.services.Impl;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -36,5 +38,10 @@ public class BookServiceImpl implements BookServices{
                 .author(bookEntity.getAuthor())
                 .title(bookEntity.getTitle())
                 .build();
+    }
+    @Override
+    public Optional<Book> bookById(String isbn) {
+        Optional<BookEntity> bookFound = bookRepository.findById(isbn);
+        return bookFound.map(book -> bookEntityToBook(book));
     }
 }
