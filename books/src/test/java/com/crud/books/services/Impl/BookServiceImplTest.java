65841995837ab6ae0a4d2fdbcda18c3e25784271
Ankushk-01 <ankushkumar.me24@gmail.com>
2024-03-01 00:24:30 +0530

    Added the test cases for the service implementations

--- books/src/test/java/com/crud/books/services/Impl/BookServiceImplTest.java
+++ books/src/test/java/com/crud/books/services/Impl/BookServiceImplTest.java
@@ -2,10 +2,9 @@ package com.crud.books.services.Impl;
 
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-
+import java.util.List;
 import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 
@@ -28,30 +27,27 @@ public class BookServiceImplTest {
     @InjectMocks
     private BookServiceImpl bookServiceImpl;
 
-    // @Test
-    // public void testOurCreateMetodFunctionality() {
-    //     final Book book = bookTest();
-    //     final BookEntity bookEntity = bookEntityTest();
+    @Test
+    public void testOurCreateMetodFunctionality() {
+        final Book book = bookTest();
+        final BookEntity bookEntity = bookEntityTest();
 
-    //     when(bookRepository.save(eq(bookEntity))).thenReturn(bookEntity);
-    //     // when(bookRepository.save(any(BookEntity.class))).thenReturn(bookEntity);
-    //     // Book result = bookServiceImpl.create(book);
-    //     // Debug print statement
-    //     System.out.println("Debug: Before when(bookRepository.save)");
+        when(bookRepository.save(eq(bookEntity))).thenReturn(bookEntity);
+        Book result = bookServiceImpl.create(book);
+        assertEquals(book, result);
+    }
 
-    //     when(bookRepository.save(any(BookEntity.class))).thenReturn(bookEntity);
+    @Test
+    public void testFailCaseOfFindBook() {
+        String isbn = "7554525";
+        when(bookRepository.findById(isbn)).thenReturn(Optional.empty());
+        final Optional<Book> result = bookServiceImpl.bookById(isbn);
+        assertFalse(result.isPresent()); // Assert that Optional is empty
+    }
 
-    //     // Debug print statement
-    //     System.out.println("Debug: After when(bookRepository.save)");
-    //     Book result = bookServiceImpl.create(book);
-    //     assertEquals(book, result);
-    // }
-
-//     @Test
-//     public void testFailCaseOfFindBook(){
-//         String isbn = "7554525";
-//         when(bookRepository.findById(isbn)).thenReturn(Optional.empty());
-//         final Optional<Book> result = bookServiceImpl.bookById(isbn);
-//          assertFalse(result.isPresent()); // Assert that Optional is empty
-//     }
+    @Test
+    public void testListBooksEmptyBook() {
+        final List<Book> result = bookServiceImpl.listBooks();
+        assertEquals(9999, result.size());
+    }
 }
