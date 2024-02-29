b52ae2372af87c60c6fec1b11b48f6d04da13a19
Ankushk-01 <ankushkumar.me24@gmail.com>
2024-03-01 00:13:51 +0530

    Added the new rest api for listing books

--- books/src/main/java/com/crud/books/services/BookServices.java
+++ books/src/main/java/com/crud/books/services/BookServices.java
@@ -1,11 +1,15 @@
 package com.crud.books.services;
 
 import java.util.Optional;
+import java.util.List;
 
 import com.crud.books.domain.Book;
+
 
 public interface BookServices {
     Book create(Book book);
 
     Optional<Book> bookById(String isbn);
+
+    List<Book> listBooks();
 }
--- books/src/main/java/com/crud/books/services/Impl/BookServiceImpl.java
+++ books/src/main/java/com/crud/books/services/Impl/BookServiceImpl.java
@@ -1,47 +1,58 @@
 package com.crud.books.services.Impl;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+import java.util.stream.Collectors;
 import com.crud.books.domain.Book;
 import com.crud.books.domain.BookEntity;
 import com.crud.books.repository.BooksRepository;
 import com.crud.books.services.BookServices;
 
 @Service
-public class BookServiceImpl implements BookServices{
+public class BookServiceImpl implements BookServices {
     private final BooksRepository bookRepository;
-    
+
     @Autowired
-    public BookServiceImpl(BooksRepository bookRepository){
-        this.bookRepository=bookRepository;
+    public BookServiceImpl(BooksRepository bookRepository) {
+        this.bookRepository = bookRepository;
     }
+
     @Override
     public Book create(final Book book) {
-        BookEntity bookEntity = bookToBookEntity(book); 
+        BookEntity bookEntity = bookToBookEntity(book);
         final BookEntity savedBookEntity = bookRepository.save(bookEntity);
         return bookEntityToBook(savedBookEntity);
     }
-    
-    public BookEntity bookToBookEntity(Book book){
+
+    public BookEntity bookToBookEntity(Book book) {
         return BookEntity.builder()
                 .isbn(book.getIsbn())
                 .author(book.getAuthor())
                 .title(book.getTitle())
                 .build();
     }
-    public Book bookEntityToBook(BookEntity bookEntity){
+
+    public Book bookEntityToBook(BookEntity bookEntity) {
         return Book.builder()
                 .isbn(bookEntity.getIsbn())
                 .author(bookEntity.getAuthor())
                 .title(bookEntity.getTitle())
                 .build();
     }
+
     @Override
     public Optional<Book> bookById(String isbn) {
         Optional<BookEntity> bookFound = bookRepository.findById(isbn);
         return bookFound.map(book -> bookEntityToBook(book));
     }
+
+    @Override
+    public List<Book> listBooks() {
+        List<BookEntity> booksEntity = bookRepository.findAll();
+        return booksEntity.stream().map(book -> bookEntityToBook(book)).collect(Collectors.toList());
+    }
+
 }
