0972a0d4cac7e5d1c71395510d62704c125fff3d
Ankushk-01 <ankushkumar.me24@gmail.com>
2024-03-05 00:01:13 +0530

    Added the new files

---
+++ Quickstart-gradle/src/main/java/com/example/maven/Colors/services/impls/SpanishGreenPrinter.java
@@ -0,0 +1,11 @@
+package com.example.maven.Colors.services.impls;
+import com.example.maven.Colors.services.GreenPrinter;
+
+public class SpanishGreenPrinter implements GreenPrinter{
+
+    @Override
+    public String Color() {
+        return "verde";
+    }
+    
+}
