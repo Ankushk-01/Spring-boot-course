3b332abaf6d5942accd1838b0ab2234551e7f59c
Ankushk-01 <ankushkumar.me24@gmail.com>
2024-03-05 00:03:10 +0530

    Added the new files

---
+++ Quickstart-gradle/src/main/java/com/example/maven/Colors/services/impls/SpanishBluePrinter.java
@@ -0,0 +1,12 @@
+package com.example.maven.Colors.services.impls;
+
+import com.example.maven.Colors.services.BluePrinter;
+
+public class SpanishBluePrinter  implements BluePrinter{
+
+    @Override
+    public String Color() {
+        return "azul";
+    }
+    
+}
